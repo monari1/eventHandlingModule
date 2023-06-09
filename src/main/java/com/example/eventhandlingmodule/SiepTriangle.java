14b35d99c414dfcf608f01b2806de1987d0a6413
monari1 <francisnyasinga@gmail.com>
2023-06-09 22:50:53 +0300

    part 2: fractals and how its visualized for any order

--- src/main/java/com/example/eventhandlingmodule/SiepTriangle.java
+++ src/main/java/com/example/eventhandlingmodule/SiepTriangle.java
@@ -1,6 +1,7 @@
 package com.example.eventhandlingmodule;
 
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -8,6 +9,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 public class SiepTriangle extends Application {
@@ -38,9 +41,50 @@ public class SiepTriangle extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        pane.widthProperty().addListener(ov -> pane.paint());
+        pane.heightProperty().addListener(ov -> pane.paint());
+
     }
 
     static class SieptrianglePane extends Pane{
+
+        private  int order = 0;
+        public void setOrder(int order){
+            this.order = order;
+            paint();
+        }
+        SieptrianglePane(){
+
+        }
+        protected void paint(){
+            Point2D p1 = new Point2D(getWidth()/2, 10);
+            Point2D p2 = new Point2D(10, getHeight()-10 );
+            Point2D p3 = new Point2D(getWidth()-10, getHeight()-10);
+
+            this.getChildren().clear();
+            displayTriangles(order, p1, p2, p3);
+        }
+        private void displayTriangles(int order, Point2D p1, Point2D p2, Point2D p3){
+            if (order == 0){
+                Polygon triangle = new Polygon();
+                triangle.getPoints().addAll(p1.getX(), p1.getY(), p2.getX(), p2.getY(), p3.getX(), p3.getY());
+                triangle.setStroke(Color.BLUE);
+                triangle.setFill(Color.AZURE);
+                this.getChildren().add(triangle);
+            }
+            else {
+                Point2D p12 = p1.midpoint(p2);
+                Point2D p23 = p2.midpoint(p3);
+                Point2D p31 = p3.midpoint(p1);
+
+//                creating the recursion code
+                displayTriangles(order-1, p1, p12, p31);
+                displayTriangles(order-1, p12, p2, p23);
+                displayTriangles(order-1, p31, p23, p3);
+
+
+            }
+        }
 
     }
 }
