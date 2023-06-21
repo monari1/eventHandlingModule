f28ee9c666ff68857657ff692c307c5aa92f58ac
monari1 <francisnyasinga@gmail.com>
2023-06-21 23:50:43 +0300

    final implementation of ball control

--- src/main/java/com/example/eventhandlingmodule/BallBounceControl.java
+++ src/main/java/com/example/eventhandlingmodule/BallBounceControl.java
@@ -1,6 +1,8 @@
 package com.example.eventhandlingmodule;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class BallBounceControl extends Application {
@@ -12,6 +14,31 @@ public class BallBounceControl extends Application {
     @Override
     public void start(Stage primaryStage) {
         BallPane ballPane = new BallPane();
+
+//        pause and resume animation
+        ballPane.setOnMousePressed(e-> ballPane.pause());
+        ballPane.setOnMouseReleased(e-> ballPane.play());
+
+//        increaese and decrease animation speed
+
+        ballPane.setOnKeyPressed(e->{
+
+            if (e.getCode()== KeyCode.UP){
+                ballPane.increaseSpeed();
+            } else if (e.getCode()== KeyCode.DOWN) {
+                ballPane.decreaseSpeed();
+
+            }
+        });
+
+        Scene scene = new Scene(ballPane, 300, 300);
+        primaryStage.setTitle("Bounce Ball Control");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+//        must request focus after the primary stage is displayed
+
+        ballPane.requestFocus();
 
     }
 }
