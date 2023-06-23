8096849aac88b8dac649714a32f4bae152644655
monari1 <francisnyasinga@gmail.com>
2023-06-23 21:36:45 +0300

    done with MapPane

--- src/main/java/com/example/eventhandlingmodule/MapPane.java
+++ src/main/java/com/example/eventhandlingmodule/MapPane.java
@@ -1,16 +1,89 @@
 package com.example.eventhandlingmodule;
 
 import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
-public class MapPane extends Application {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        launch(args);
+public class MapPane extends BorderPane {
+    private Group group = new Group();
+    MapPane(){
+        ArrayList<ArrayList<Point2D>> points = getPoints();
+        for (int i = 0; i<points.size();i++ ){
+            Polygon polygon = new Polygon();
+
+            for (int j = 0; j < points.get(i).size(); j++){
+                polygon.getPoints().addAll(points.get(i).get(j).getX(), -points.get(i).get(j).getY());
+                polygon.setFill(Color.WHITE);
+                polygon.setStroke(Color.BLACK);
+                polygon.setStrokeWidth(1/14.0);
+
+                polygon.setOnMouseClicked(e->{
+
+                    if (e.getButton()== MouseButton.PRIMARY){
+                        polygon.setFill(Color.RED);
+
+                    } else if (e.getButton()== MouseButton.SECONDARY) {
+                        polygon.setFill(Color.BLUE);
+
+                    }
+                    else {
+                        polygon.setFill(Color.WHITE);
+                    }
+
+                });
+                group.getChildren().add(polygon);
+            }
+            group.setScaleX(14);
+            group.setScaleY(14);
+            this.setCenter(group);
+        }
+
+    }
+    public void enlarge(){
+        group.setScaleX(1.1*group.getScaleX());
+        group.setScaleY(1.1*group.getScaleY());
+
+
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    public void shrink(){
+        group.setScaleX(0.9*group.getScaleX());
+        group.setScaleY(0.9*group.getScaleY());
+
 
     }
+
+    private ArrayList<ArrayList<Point2D>> getPoints(){
+        ArrayList<ArrayList<Point2D>> points = new ArrayList<>();
+        try (Scanner input = new Scanner(new java.net.URL("https://liveexample.pearsoncmg.com/data/usmap.txt").openStream())) {
+            while (input.hasNext()){
+                String s = input.nextLine();
+                if (Character.isAlphabetic(s.charAt(0))){
+                    points.add(new ArrayList<>());
+                }
+                else {
+                    Scanner scanAString = new Scanner(s);
+                    double y = scanAString.nextDouble();
+                    double x = scanAString.nextDouble();
+                    points.get(points.size()-1).add(new Point2D(x, y));
+                }
+            }
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        return points;
+    }
+
+
 }
