a4e2c4d8151858eefcb0de09d10eb20be352ca73
monari1 <francisnyasinga@gmail.com>
2023-06-23 21:47:30 +0300

    the implementation of USMAP

--- src/main/java/com/example/eventhandlingmodule/USMap.java
+++ src/main/java/com/example/eventhandlingmodule/USMap.java
@@ -1,6 +1,8 @@
 package com.example.eventhandlingmodule;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class USMap extends Application {
@@ -11,6 +13,18 @@ public class USMap extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        MapPane map = new MapPane();
+        Scene scene = new Scene(map, 1200, 800);
+        primaryStage.setTitle("US MAP BY COLOR");
+        map.setOnKeyPressed(e->{
+            if (e.getCode()== KeyCode.UP){
+                map.enlarge();
+
+            } else if (e.getCode()== KeyCode.DOWN){
+                map.shrink();}
+        });
+map.requestFocus();
 
     }
+
 }
